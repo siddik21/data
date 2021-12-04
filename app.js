23b165f37970a95ae9688604ec292c30b7b4f671
Siddik <siddikcse21@gmail.com>
2021-12-05 00:54:23 +0600

    upload this

---
+++ app.js
@@ -0,0 +1,364 @@
+const lfwfAcademy=[
+    {
+        id: 101,
+        name: 'Abu Bakkar Siddik',
+        department: 'MERN Stack',
+        gendar: 'Male',
+        location: 'Mirpur'
+    },
+    {
+        id: 102,
+        name: 'Delwar Hossain',
+        department: 'Java Script',
+        gendar: 'Male',
+        location: 'Banani'
+    },
+    {
+        id: 103,
+        name: 'Jillur Rahman',
+        department: 'CEO',
+        gendar: 'Male',
+        location: 'Dinajpur'
+    },
+    {
+        id: 104,
+        name: 'Basudeb',
+        department: 'Java',
+        gendar: 'Male',
+        location: 'Jossor'
+    },
+    {
+        id: 105,
+        name: 'Jhilik',
+        department: 'Web Design',
+        gendar: 'Female',
+        location: 'Shemoly'
+    },
+    {
+        id: 106,
+        name: 'Emon Chowdhury',
+        department: 'CEO',
+        gendar: 'Male',
+        location: 'Banani'
+    },
+    {
+        id: 107,
+        name: 'Shanto',
+        department: 'Networking',
+        gendar: 'Male',
+        location: 'Adabor'
+    },
+    {
+        id: 108,
+        name: 'Sumon',
+        department: 'Project',
+        gendar: 'Male',
+        location: 'Gulsan'
+    },
+    {
+        id: 109,
+        name: 'Sohan',
+        department: 'YouTube',
+        gendar: 'Male',
+        location: 'Mirpur'
+    },
+    {
+        id: 110,
+        name: 'Mostafiz',
+        department: 'Networking',
+        gendar: 'Male',
+        location: 'Mohammodpur'
+    },
+    {
+        id: 111,
+        name: 'Habib',
+        department: 'Graphics',
+        gendar: 'Male',
+        location: 'Mirpur'
+    },
+    {
+        id: 112,
+        name: 'Tuhin',
+        department: 'Java Script',
+        gendar: 'Male',
+        location: 'Banani'
+    },
+    {
+        id: 113,
+        name: 'Mamun',
+        department: 'COO',
+        gendar: 'Male',
+        location: 'Dinajpur'
+    },
+    {
+        id: 114,
+        name: 'Deb',
+        department: 'Java',
+        gendar: 'Male',
+        location: 'Jossor'
+    },
+    {
+        id: 115,
+        name: 'Neha',
+        department: 'Web Design',
+        gendar: 'Female',
+        location: 'Shemoly'
+    },
+    {
+        id: 116,
+        name: 'Mehedi',
+        department: 'Ower',
+        gendar: 'Male',
+        location: 'Banani'
+    },
+    {
+        id: 117,
+        name: 'Shanto Khan',
+        department: 'Networking',
+        gendar: 'Male',
+        location: 'Hili'
+    },
+    {
+        id: 118,
+        name: 'Sumona',
+        department: 'Project',
+        gendar: 'Female',
+        location: 'Gulsan'
+    },
+    {
+        id: 119,
+        name: 'Din',
+        department: 'Civil',
+        gendar: 'Male',
+        location: 'Mirpur'
+    },
+    {
+        id: 120,
+        name: 'Pollob',
+        department: 'Web',
+        gendar: 'Male',
+        location: 'Mohammodpur'
+    },
+    {
+        id: 121,
+        name: 'Siddik',
+        department: 'MERN Stack',
+        gendar: 'Male',
+        location: 'Sripur'
+    },
+    {
+        id: 122,
+        name: 'Iqbal Hossain',
+        department: 'Java Script',
+        gendar: 'Male',
+        location: 'Khulna'
+    },
+    {
+        id: 123,
+        name: 'Pias',
+        department: 'Marketing',
+        gendar: 'Male',
+        location: 'Dinajpur'
+    },
+    {
+        id: 124,
+        name: 'Jit',
+        department: 'Graphics',
+        gendar: 'Male',
+        location: 'Jossor'
+    },
+    {
+        id: 125,
+        name: 'Puja',
+        department: 'PHP',
+        gendar: 'Female',
+        location: 'Banani'
+    },
+    {
+        id: 126,
+        name: 'Iqbal Khan',
+        department: 'Daroan',
+        gendar: 'Male',
+        location: 'Bolorampur'
+    },
+    {
+        id: 127,
+        name: 'Saju Mama',
+        department: 'Security',
+        gendar: 'Male',
+        location: 'Suihari'
+    },
+    {
+        id: 128,
+        name: 'Sumona',
+        department: 'Project',
+        gendar: 'Female',
+        location: 'Mirpur'
+    },
+    {
+        id: 129,
+        name: 'Sohana',
+        department: 'Web',
+        gendar: 'Female',
+        location: 'Mirpur'
+    },
+    {
+        id: 130,
+        name: 'Tisha',
+        department: 'BBA',
+        gendar: 'Female',
+        location: 'Farmgate'
+    },
+    {
+        id: 131,
+        name: 'Pijus Da',
+        department: 'English',
+        gendar: 'Male',
+        location: 'Mirpur'
+    },
+    {
+        id: 132,
+        name: 'Akram',
+        department: 'Laravel',
+        gendar: 'Male',
+        location: 'Mirpur'
+    },
+    {
+        id: 133,
+        name: 'Arnika',
+        department: 'PHP',
+        gendar: 'Female',
+        location: 'Banani'
+    },
+    {
+        id: 134,
+        name: 'Debi',
+        department: 'PHP',
+        gendar: 'Female',
+        location: 'Rator'
+    },
+    {
+        id: 135,
+        name: 'Promi',
+        department: 'Web Design',
+        gendar: 'Female',
+        location: 'Banani'
+    },
+    {
+        id: 136,
+        name: 'Sahinur',
+        department: 'Night Gaurd',
+        gendar: 'Male',
+        location: 'Dhaka'
+    },
+    {
+        id: 137,
+        name: 'Sohana',
+        department: 'Web',
+        gendar: 'Female',
+        location: 'Kolkata'
+    },
+    {
+        id: 138,
+        name: 'Sabbir',
+        department: 'Project',
+        gendar: 'Male',
+        location: 'Gulsan'
+    },
+    {
+        id: 139,
+        name: 'Pal',
+        department: 'PHP',
+        gendar: 'Male',
+        location: 'Mirpur'
+    },
+    {
+        id: 140,
+        name: 'Mostofa',
+        department: 'Harware',
+        gendar: 'Male',
+        location: 'Banani'
+    },
+    {
+        id: 141,
+        name: 'Abu Bakkar',
+        department: 'Java Script',
+        gendar: 'Male',
+        location: 'Dhaka'
+    },
+    {
+        id: 142,
+        name: 'Delwar khan',
+        department: 'Java Script',
+        gendar: 'Male',
+        location: 'Piganj'
+    },
+    {
+        id: 143,
+        name: 'Mojibor Rahman',
+        department: 'MD',
+        gendar: 'Male',
+        location: 'Banani'
+    },
+    {
+        id: 144,
+        name: 'Mahi',
+        department: 'Java',
+        gendar: 'Male',
+        location: 'Ranisonkail'
+    },
+    {
+        id: 145,
+        name: 'Meraz',
+        department: 'Web Design',
+        gendar: 'Male',
+        location: 'Shemoly'
+    },
+    {
+        id: 146,
+        name: 'Azom',
+        department: 'COO',
+        gendar: 'Male',
+        location: 'Rajshahi'
+    },
+    {
+        id: 147,
+        name: 'Shantona',
+        department: 'Graphics',
+        gendar: 'Female',
+        location: 'Khonjona'
+    },
+    {
+        id: 148,
+        name: 'Suborna',
+        department: 'Project',
+        gendar: 'Female',
+        location: 'Mohammodpur'
+    },
+    {
+        id: 149,
+        name: 'Sohana Khanom',
+        department: 'Web',
+        gendar: 'Female',
+        location: 'Mirpur'
+    },
+    {
+        id: 150,
+        name: 'Toni',
+        department: 'Texttail',
+        gendar: 'Male',
+        location: 'Tejgaon'
+    }
+    
+]
+
+lfwfAcademy.map(data => {
+    if(data.location == 'Mirpur' || data.location == 'Banani'){
+        console.log(`
+        ID: ${data.id},
+        Name: ${data.name},
+        Department: ${data.department},
+        Location: ${data.location}
+        `)
+    }
+})
